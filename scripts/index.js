5af563eb5de9373f281ce67105cd5dbbe723846c
Samuel-UC <68044712+Samuel-UC@users.noreply.github.com>
2021-01-27 19:57:05 -0800

    typing subtitle code

--- scripts/index.js
+++ scripts/index.js
@@ -1,107 +1,163 @@
-let timeline = gsap.timeline(), 
-    chars = document.querySelectorAll('.header-animation > div > div'); 
+class Typer {
+    constructor(element) {
+        this.element = element;
+        this.element.classList.add('typing-cursor');
+
+        this.index = 0;
+        this.chars = 'qwertyuiopasdfghjklzxcvbnm';
+    }
+
+    type_loop(resolve, text, speed, accuracy) {
+
+        if (this.index >= text.length &&
+            this.element.textContent == text) return resolve();
+
+        if (this.element.textContent.slice(0, this.index) != text.slice(0, this.index)) {
+
+            this.element.textContent = this.element.textContent.slice(0, -1);
+            this.index--;
+
+            return setTimeout(() => this.type_loop.apply(this, arguments), speed);
+        }
+
+        const is_mistake = Math.random() < accuracy;
+
+        this.element.textContent += is_mistake ?
+            this.chars[Math.floor(Math.random() * this.chars.length)] :
+            text[this.index];
+
+        this.index++;
+
+        return setTimeout(() => this.type_loop.apply(this, arguments), speed * (is_mistake ? 3 : 1));
+    }
+
+    clean_loop(resolve, speed) {
+        if (this.element.textContent == '')
+            return resolve();
+
+        this.element.textContent = this.element.textContent.slice(0, -1);
+        return setTimeout(() => this.clean_loop.apply(this, arguments), speed);
+    }
+
+    clean(speed = 30) {
+        return new Promise(resolve => this.clean_loop(resolve, speed));
+    }
+
+    type(text, speed = 30, accuracy = 0.05) {
+        return new Promise(resolve => this.type_loop(resolve, text, speed, accuracy));
+    }
+}
+
+const style = document.createElement('style');
+style.textContent = `.typing-cursor::after { animation: cursor_blink 1s steps(1) infinite; display: inline-block; content: '_'; } @keyframes cursor_blink { 0% { opacity: 1; } 50% { opacity: 0; } 100% { opacity: 1; } }`;;
+document.head.appendChild(style);
+
+const handle = new Typer(document.getElementById('titles'));
+
+let typing = ['3+ years of backend experience', '3+ years of UI/UX design', '3+ years of game development', '5+ years of frontend experience', 'software engineer', '3D artist'],
+    index = 0;
+
+const loop = () => {
+
+    if (index >= typing.length) index = 0;
+
+    handle.clean()
+        .then(() => {
+          handle.type(typing[index++], 50, 0.03)
+          .then(() => setTimeout(loop, 4000));
+        });
+    
+};
 
 
-timeline.from(chars, { duration: 0.5, opacity: 0, scale: 0, y: 80, rotationX: 180, transformOrigin: '0% 50% -50',  ease: 'back', stagger: 0.1 });
 
+let timeline = gsap.timeline(),
+    chars = document.querySelectorAll('.header-animation > div > div');
+
+
+timeline.from(chars, { duration: 0.5, opacity: 0, scale: 0, y: 80, rotationX: 180, transformOrigin: '0% 50% -50', ease: 'back', stagger: 0.1 });
+timeline.call(loop);
 
 // Toggle menu
-document.getElementById('toggle-menu').addEventListener('click', function () {
+document.getElementById('toggle-menu').addEventListener('click', function() {
     this.classList.toggle('clicked');
 });
 
 particlesJS('header', {
     "particles": {
-      "number": {
-        "value": 100,
-        "density": {
-          "enable": true,
-          "value_area": 500
+        "number": {
+            "value": 100,
+            "density": {
+                "enable": true,
+                "value_area": 500
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 3,
+                "size_min": 0.8,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": false,
+            "distance": 0,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 1,
+            "direction": "top",
+            "random": true,
+            "straight": true,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 0,
+                "rotateY": 0
+            }
         }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 3,
-          "size_min": 0.8,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": false,
-        "distance": 0,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 1,
-        "direction": "top",
-        "random": true,
-        "straight": true,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 0,
-          "rotateY": 0
-        }
-      }
     },
     "interactivity": {
-      "detect_on": "window",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "bubble"
+        "detect_on": "window",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "bubble"
+            },
+            "onclick": {
+                "enable": false
+            },
+            "resize": true
         },
-        "onclick": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 250,
-          "size": 3,
-          "duration": 5,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 100,
-          "duration": 10
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
+        "modes": {
+            "bubble": {
+                "distance": 250,
+                "size": 3,
+                "duration": 5,
+                "opacity": 8,
+                "speed": 3
+            }
         }
-      }
     },
     "retina_detect": true
 });
