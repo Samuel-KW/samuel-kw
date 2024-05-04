8121cde29090f4009a35c0a2dbaf152035be3c39
Samuel-UC <68044712+Samuel-KW@users.noreply.github.com>
2024-05-04 01:14:16 -0700

    Refactor terminal.js to improve user interaction and add new commands

--- scripts/terminal.js
+++ scripts/terminal.js
@@ -1,3 +1,4 @@
+
 class Terminal {
     
     constructor(terminalElement) {
@@ -6,11 +7,20 @@ class Terminal {
         this._lines = [];
 
         this.inputElem = this.addLine("", this.path, this.user, true);
-        console.log(terminalElement);
-        terminalElement.addEventListener("click", () => this.inputElem._inputElem.focus());
+        terminalElement.addEventListener("mouseup", e => {
+            if (this.enabled)
+                this.inputElem._inputElem.focus();
+        });
+
+        terminalElement.addEventListener("mousedown", e => {
+            if (!this.enabled)
+                e.preventDefault();
+        });
 
         this.path = "~/index.html";
         this.user = "Guest@Portfolio";
+
+        this.enabled = false;
     }
 
     get firstLine() {
@@ -32,9 +42,25 @@ class Terminal {
                 this.addOutputLine("clear - clear the terminal");
                 break;
 
+            case "cls":
             case "clear":
                 this._lines.forEach(line => line.remove());
                 this._lines = [];
+                break;
+
+            case "ls":
+                this.addOutputLine("index.html");
+                this.addOutputLine("about.html");
+                this.addOutputLine("projects.html");
+                this.addOutputLine("contact.html");
+                break;
+
+            case "whoami":
+                this.addOutputLine("Samuel Walls");
+                break;
+
+            case "echo":
+                this.addOutputLine(args.slice(1).join(" "));
                 break;
 
             default:
@@ -65,8 +91,14 @@ class Terminal {
         this._lines.push(line);
         this._parent.appendChild(line);
 
-        line._inputElem.focus();
         line._inputElem.addEventListener("keydown", e => {
+
+            if (!this.enabled) {
+                e.stopPropagation();
+                e.preventDefault();
+                return;
+            }
+
             if (e.code === "Enter") {
                 e.preventDefault();
                 this.handleInput(line);
@@ -75,11 +107,25 @@ class Terminal {
 
         return line;
     }
-
 }
 
-(async () => {
+(() => {
+
     const terminal = new Terminal(document.querySelector("#content .terminal-body-content"));
     window.terminal = terminal;
-    
+
+    document.addEventListener("DOMContentLoaded", async () => {
+
+        await delay(1000);
+        terminal.inputElem.type("whoami");
+        await delay(1000);
+        terminal.handleInput(terminal.inputElem);
+        await delay(3000);
+        
+        terminal.inputElem.type("help");
+        await delay(1000)
+        terminal.handleInput(terminal.inputElem);
+
+        terminal.enabled = true;
+    });
 })();
