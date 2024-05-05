cbb3c52dd7374006e7b75d59960730dd6834c0d4
Samuel-UC <68044712+Samuel-KW@users.noreply.github.com>
2024-05-05 10:19:02 -0700

    Optimized ASCII to image, commented code

--- effect/imageEffect.js
+++ effect/imageEffect.js
@@ -19,39 +19,60 @@ class AsciiImage extends HTMLElement {
     }
     
     loadImage (img) {
+
+        // Draw the image to the canvas
         this._ctx.drawImage(img, 0, 0);
-            
+        
+        // Get the image data
         const data = this._ctx.getImageData(0, 0, img.width, img.height);
         const pixels = data.data;
 
+        // Clear the canvas before drawing
         this._ctx.clearRect(0, 0, img.width, img.height);
 
+        // Width and height of each chunk
         const chunkWidth = 15;
         const chunkHeight = 20;
 
+        // How many pixels to skip
+        const skip = 5;
+
+        // Divide image into chunks
         const rowChunks = Math.ceil(data.width / chunkWidth); // Each X chunk is 15 pixels wide
         const columnChunks = Math.ceil(data.height / chunkHeight); // Each Y chunk is 20 pixels tall
 
+        // Loop through each chunk
         for (let i = 0; i < rowChunks * columnChunks; ++i) {
             
+            // Get the chunk's position
             const chunkX = i % rowChunks;
             const chunkY = Math.floor(i / rowChunks);
 
-            let avg = 0;
+            // Average grayscale color of the chunk
+            let avg = 0,
+                total = 0;
 
             // Loop through each pixel in the chunk
-            for (let j = 0; j < chunkHeight; ++j) {
-                for (let k = 0; k < chunkWidth; ++k) {
-                    const x = chunkX * chunkWidth + k;
-                    const y = chunkY * chunkHeight + j;
+            for (let j = 0; j < chunkHeight; j += skip) {
 
+                // Get pixel position from chunk Y position
+                const y = chunkY * chunkHeight + j;
+
+                for (let k = 0; k < chunkWidth; k += skip) {
+
+                    // Get pixel position from chunk X position
+                    const x = chunkX * chunkWidth + k;
+
+                    // Get index of pixel in the data array
                     const index = (y * data.width + x) * 4;
 
                     // Optimally convert to grayscale
                     const grayscale = pixels[index] * 0.299 + pixels[index + 1] * 0.587 + pixels[index + 2] * 0.114;
                     if (isNaN(grayscale)) continue;
 
+                    // Add value to the average
                     avg += grayscale;
+                    total++;
 
                     // Set the new pixel values
                     pixels[index] = grayscale;
@@ -60,23 +81,16 @@ class AsciiImage extends HTMLElement {
                 }
             }
 
-            avg /= chunkWidth * chunkHeight;
+            // Average the chunk colors
+            avg /= total;
             
+            // Calculate the threshold for each character
             const threshold = 256 / this.gradient.length;
             
             // write ascii character to canvas at position
             this._ctx.font = "20px consolas";
             this._ctx.fillStyle = 'black';
             this._ctx.fillText(this.gradient[Math.floor(avg / threshold)], chunkX * chunkWidth, chunkY * chunkHeight);
-            
-
-            // // Optimally convert to grayscale
-            // const grayscale = pixels[i] * 0.299 + pixels[i + 1] * 0.587 + pixels[i + 2] * 0.114;
-
-            // // Set the new pixel values
-            // pixels[i] = grayscale;
-            // pixels[i + 1] = grayscale;
-            // pixels[i + 2] = grayscale;
         }
 
         return data;
@@ -130,17 +144,6 @@ class AsciiImage extends HTMLElement {
         shadow.appendChild(style);
         shadow.appendChild(this._c);
         shadow.appendChild(this._img);
-    }
-    
-    convertToAscii(imageData, width) {
-        let ascii = "";
-        for (let i = 0; i < imageData.length; i += 4) {
-        const brightness = (imageData[i] + imageData[i + 1] + imageData[i + 2]) / 3;
-        const index = Math.floor((brightness / 255) * (gradient.length - 1));
-        ascii += gradient[index];
-        if ((i / 4) % width === 0) ascii += "\n";
-        }
-        return ascii;
     }
 }
 window.customElements.define("ascii-img", AsciiImage);
