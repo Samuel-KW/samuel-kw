0636afb5b0901f5b3c4be4c18e3bf969db56ba63
Samuel W <68044712+Samuel-KW@users.noreply.github.com>
2024-08-12 11:21:52 -1000

    fix: added color theme support + fixed css modules

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,8 +1,10 @@
+import styles from "./Background.module.css";
+
 export default function Background() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 16" className="bg-path" height="100%">
-            <path d="m 0 0 l 12 0 c -10 7 1 12 -4 16 l -8 0" className="bg-path-outer" />
-            <path d="m 0 0 l 11 0 c -10 7 1 12 -4 16 l -7 0" className="bg-path-inner" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 16" className={styles.bg} height="100%">
+            <path d="m 0 0 l 12 0 c -10 7 1 12 -4 16 l -8 0" className={styles.bgOuter} />
+            <path d="m 0 0 l 11 0 c -10 7 1 12 -4 16 l -7 0" className={styles.bgInner} />
         </svg>
     );
 };
