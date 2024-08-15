4cdcd87f2f4d367b08cf551640229c7a938944a3
Samuel-UC <68044712+Samuel-KW@users.noreply.github.com>
2024-08-15 12:01:15 -0700

    feat: finished background svg styles

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -2,7 +2,7 @@ import styles from "./Background.module.css";
 
 export default function Background() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 16" className={styles.bg} height="100%">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 16" preserveAspectRatio="none" className={styles.bg}>
             <path d="m 0 0 l 12 0 c -10 7 1 12 -4 16 l -8 0" className={styles.bgOuter} />
             <path d="m 0 0 l 11 0 c -10 7 1 12 -4 16 l -7 0" className={styles.bgInner} />
         </svg>
