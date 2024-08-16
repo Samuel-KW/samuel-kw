3f05acb0787ec4905b35c5eb1d6f3e1dc36b029f
Samuel-UC <68044712+Samuel-KW@users.noreply.github.com>
2024-08-15 17:14:37 -0700

    feat: control button styles

--- src/components/console/Terminal.tsx
+++ src/components/console/Terminal.tsx
@@ -49,22 +49,71 @@ export interface TerminalProps {
     lines: TerminalLine[];
 }
 
+import { useState } from "react";
 import styles from "./Terminal.module.css";
+
+interface ControlProps {
+    onClick(): void;
+}
+
+function Minimize (_props: Partial<ControlProps>) {
+    const onClick = _props.onClick || (() => {});
+
+    return <div className={styles.minimize} onClick={onClick}></div>
+}
+
+function Expand (_props: Partial<ControlProps>) {
+    const onClick = _props.onClick || (() => {});
+
+    return <div className={styles.expand} onClick={onClick}></div>
+}
+
+function Close (_props: Partial<ControlProps>) {
+    const onClick = _props.onClick || (() => {});
+
+    return <div className={styles.close} onClick={onClick}></div>
+}
 
 export default function Terminal (_props: Partial<TerminalProps>) {
 
-    return <div className={styles.terminal}>
+    const [opened, setOpened] = useState(true);
+    const [maximized, setMaximized] = useState(false);
+    const [minimized, setMinimized] = useState(false);
+
+    return <div className={styles.terminal + " " + (maximized ? styles.maximized : "") + " " + (minimized ? styles.minimized : "")} style={{ opacity: opened ? 1 : 0 }}>
         <div className={styles.bar}>
             <span className={styles.header}>Portfolio - Samuel Walls</span>
             <span className={styles.controls}>
-                <span className={styles.minimize}></span>
-                <span className={styles.expand}></span>
-                <span className={styles.close}></span>
+                <Minimize onClick={() => setMinimized(!minimized)} />
+                <Expand onClick={() => setMaximized(!maximized)} />
+                <Close onClick={() => setOpened(false)} />
             </span>
         </div>
 
         <div className={styles.body}>
             <div className={styles.content}>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p>Lorum ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
                 {/* <ascii-img src="../images/day 3.webp" width="1920" height="1080" scale=".25" color="#ffffff" alt="Cinematic picture of a field of dandylions against a snow-capped mountain range."></ascii-img> */}
             </div>
         </div>
