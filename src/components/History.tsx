25ec031bbdfff8060da070525dcfdba67011e6e0
Samuel W <68044712+Samuel-KW@users.noreply.github.com>
2024-08-17 22:32:54 -0700

    feat: add auto-complete and history helpers

---
+++ src/components/History.tsx
@@ -0,0 +1,75 @@
+/**
+ * A class for managing the command history
+ * @example
+ * const terminalHistory = new History();
+ * 
+ * terminalHistory.addCommand('ls');
+ * terminalHistory.addCommand('cd /');
+ * terminalHistory.addCommand('pwd');
+ * 
+ * console.log(terminalHistory.getPreviousCommand()); // Output: "pwd"
+ * console.log(terminalHistory.getPreviousCommand()); // Output: "cd /"
+ * console.log(terminalHistory.getNextCommand());     // Output: "pwd"
+ * console.log(terminalHistory.getNextCommand());     // Output: null (no further history)
+ * 
+ * terminalHistory.resetHistory();
+ * console.log(terminalHistory.getHistoryState());    // Output: []
+ */
+export default class History {
+    private history: string[];
+    private currentIndex: number;
+    private currentInput: string;
+
+    constructor() {
+        this.history = [];
+        this.currentIndex = -1;
+        this.currentInput = "";
+    }
+
+    // Adds a command to the history
+    addCommand(command: string): void {
+        command = command.trim();
+
+        if (command) {
+            this.history.push(command);
+            this.currentIndex = this.history.length;
+            this.currentInput = "";
+        }
+    }
+
+    // Handles the 'up arrow' key press, returning the previous command
+    getPreviousCommand(): string | null {
+        if (this.history.length === 0 || this.currentIndex <= 0)
+            return null;
+        
+        if (this.currentIndex === this.history.length)
+            this.currentInput = "";
+        
+        this.currentIndex--;
+        return this.history[this.currentIndex];
+    }
+
+    // Handles the 'down arrow' key press, returning the next command
+    getNextCommand(): string | null {
+        if (this.history.length === 0 || this.currentIndex >= this.history.length - 1)
+            return null;
+        
+        this.currentIndex++;
+        if (this.currentIndex === this.history.length)
+            return this.currentInput;
+        
+        return this.history[this.currentIndex];
+    }
+
+    // Resets the command history to the initial state
+    resetHistory(): void {
+        this.history = [];
+        this.currentIndex = -1;
+        this.currentInput = "";
+    }
+
+    // Returns the current state of the history
+    getHistoryState(): string[] {
+        return [...this.history];
+    }
+}
