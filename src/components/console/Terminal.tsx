0b72da4d16d4382346df4e688f61e1c0adbe8261
Samuel-UC <68044712+Samuel-KW@users.noreply.github.com>
2024-09-13 16:49:11 -0700

    feat: implement draggable element

--- src/components/console/Terminal.tsx
+++ src/components/console/Terminal.tsx
@@ -6,7 +6,7 @@ import TerminalLine from "./TerminalLine";
 import TerminalInput from "./TerminalInput";
 import Welcome from "./commands/welcome";
 import Typer from "../utils/DynamicType";
-
+import Draggable from "../utils/draggable";
 
 export interface Terminal {
 
@@ -46,16 +46,21 @@ export default function Terminal (_props: Partial<TerminalProps>) {
     const typingElement = useRef(null);
     const typerRef = useRef<Typer | null>(null);
 
+    const dragElement = useRef(null);
+    const dragBar = useRef(null);
+
     useEffect(() => {
         if (!typerRef.current) {
             typerRef.current = new Typer(typingElement);
             typerRef.current.playSound = true;
             typerRef.current.startTyping(['Hello, World!', 'TypeScript is awesome!']);
         }
+
+        Draggable({ containerRef: dragElement, dragItemRef: dragBar });
     }, []);
 
-    return <div className={styles.terminal + " " + (maximized ? styles.maximized : "") + " " + (minimized ? styles.minimized : "")} style={{ opacity: opened ? 1 : 0 }} role="window" aria-label="Terminal window">
-        <div className={styles.bar}>
+    return <div ref={dragElement} className={styles.terminal} style={{ opacity: opened ? 1 : 0 }} role="window" aria-label="Terminal window">
+        <div ref={dragBar} className={styles.bar}>
             <span className={styles.header}>Portfolio - Samuel Walls</span>
             <span className={styles.controls}>
                 <Minimize onClick={() => setMinimized(!minimized)} />
@@ -67,8 +72,8 @@ export default function Terminal (_props: Partial<TerminalProps>) {
         <div className={styles.body}>
             <div className={styles.content}>
                 <Welcome />
-                <TerminalLine user="admin" directory={window.location.pathname} reference={typingElement}>Welcome to my website!</TerminalLine>
-                <TerminalInput user="admin" directory={window.location.pathname} onSubmit={console.log}/>
+                <TerminalLine user="root" directory={window.location.pathname} reference={typingElement}>Welcome to my website!</TerminalLine>
+                <TerminalInput user="root" directory={window.location.pathname} onSubmit={console.log}/>
                 {/* <ascii-img src="../images/day 3.webp" width="1920" height="1080" scale=".25" color="#ffffff" alt="Cinematic picture of a field of dandylions against a snow-capped mountain range."></ascii-img> */}
             </div>
         </div>
